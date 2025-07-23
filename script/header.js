ec8c80cad12482d776876fb7713134e5bcb278a1
davi-amorim-mendes <davi.amorim.mendes.082@gmail.com>
2025-07-23 16:23:07 -0300

    correcao do tempo de transicao header

--- script/header.js
+++ script/header.js
@@ -1,13 +1,11 @@
 window.addEventListener('scroll', function()
 {
-    console.log(window.scrollY);
-
     const headerNoBack = document.getElementById('headerNoBack');
     const headerBack = document.getElementById('headerBack');
     
     var scrollV = window.scrollY;
 
-    if(scrollV > 0)
+    if(scrollV > 10)
     {
         headerNoBack.id = "headerBack";
     }
