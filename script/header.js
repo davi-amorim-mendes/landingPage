941ebcdf235846b6072f9e2f7dd8704a034726ef
davi-amorim-mendes <davi.amorim.mendes.082@gmail.com>
2025-07-23 16:13:32 -0300

    adicao de animacao no header

---
+++ script/header.js
@@ -0,0 +1,18 @@
+window.addEventListener('scroll', function()
+{
+    console.log(window.scrollY);
+
+    const headerNoBack = document.getElementById('headerNoBack');
+    const headerBack = document.getElementById('headerBack');
+    
+    var scrollV = window.scrollY;
+
+    if(scrollV > 90)
+    {
+        headerNoBack.id = "headerBack";
+    }
+    else
+    {
+        headerBack.id = "headerNoBack";
+    }
+})
