c79c62dc357a3f7cb8edadd04233b8541e098bea
davi-amorim-mendes <davi.amorim.mendes.082@gmail.com>
2025-07-24 15:46:46 -0300

    correcao na margem da foto e otimizacao do header

--- script/header.js
+++ script/header.js
@@ -1,24 +1,27 @@
 window.addEventListener('scroll', function()
 {
-    const headerNoBack = document.getElementById('headerNoBack');
-    const headerBack = document.getElementById('headerBack');
+    const header = document.querySelector('.header')
+    // const headerBack = document.getElementById('headerBack');
     const larguraTela = window.innerWidth;
     
     var scrollV = window.scrollY;
 
     if(larguraTela <= 820)
     {
-        headerNoBack.id = "headerBack";
+        header.id = "headerBack";
     }
     else
     {
         if(scrollV > 90)
         {
-            headerNoBack.id = "headerBack";
+            header.id = "headerBack";
         }
         else
         {
-            headerBack.id = "headerNoBack";
+            header.id = "headerNoBack";
         }
     }
 })
+
+// const lang = document.documentElement.lang; 
+// PEGA O IDIOMA DA PÁGINA
